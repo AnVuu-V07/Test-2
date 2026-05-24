d9bec28e2c2fc1b03a09cd0a19e5aad1241417f4
An Vuu <anbaothanhvuu@gmail.com>
2026-05-24 20:48:49 +0700

    Hihi QB

--- Main.cpp
+++ Main.cpp
@@ -7,7 +7,13 @@ vector<int> TaoChuoi(int n){
 }
 
 void SapXepChuoi(vector<int> &DS){
-
+    for(int i = 0; i < DS.size() - 1; i++){
+        for(int j = 0; j < DS.size() - i - 1; j++){
+            if(DS[j] > DS[j + 1]){
+                swap(DS[j], DS[j + 1]);
+            }
+        }
+    }
 }
 
 int main(){
